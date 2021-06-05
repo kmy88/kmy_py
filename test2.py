f074996484ce63b11b4e67ebca9960967da2f672
kmy88 <natebbong@naver.com>
2021-06-06 01:48:09 +0900

    yaml 저장 함수구현
    yaml 저장 함수구현

--- test2.py
+++ test2.py
@@ -1,20 +1,24 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import yaml
 
 form_class = uic.loadUiType("test.ui")[0]
+
+a=20
 
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
-        
          
          
         #버튼에 기능을 할당하는 코드
         self.lineEdit_2.textChanged.connect(self.lineeditTextFunction)
         self.lineEdit_2.returnPressed.connect(self.printTextFunction)
-        self.pushButton.clicked.connect(self.changeTextFunction)
+        #self.pushButton.clicked.connect(self.changeTextFunction)
+        #line Edit에 입력된 값을 저장
+        self.pushButton.clicked.connect(self.saveText)
 
     def lineeditTextFunction(self) :
         self.lineEdit_1.setText(self.lineEdit_2.text())
@@ -27,13 +31,23 @@ class WindowClass(QMainWindow, form_class) :
         #Lineedit에 있는 글자를 가져오는 메서드
         print(self.lineEdit_2.text())
 
+       
     def changeTextFunction(self) :
         #self.lineedit이름.setText("String")
         #Lineedit의 글자를 바꾸는 메서드
         pwd_type1=(self.lineEdit_2.text())
         print(pwd_type1)
+        print(a)
         #self.lineEdit.setText("Change Text")
-        
+    
+    
+    def saveText(self) :
+        #YAML 저장을 함수로 구현 
+        pwd_type1=(self.lineEdit_2.text())
+        with open('account.yml', 'w') as f:
+              yaml.dump(pwd_type1, f) 
+     
+  
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
