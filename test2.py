b8349c8d166135caae0e5765d1b5a0c5145870e1
kmy88 <natebbong@naver.com>
2021-06-06 01:02:31 +0900

    UI명변경 test
    test

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-form_class = uic.loadUiType("lineeditTest.ui")[0]
+form_class = uic.loadUiType("test.ui")[0]
 
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
@@ -12,12 +12,12 @@ class WindowClass(QMainWindow, form_class) :
          
          
         #버튼에 기능을 할당하는 코드
-        self.lineEdit.textChanged.connect(self.lineeditTextFunction)
-        self.lineEdit.returnPressed.connect(self.printTextFunction)
+        self.lineEdit_2.textChanged.connect(self.lineeditTextFunction)
+        self.lineEdit_2.returnPressed.connect(self.printTextFunction)
         self.pushButton.clicked.connect(self.changeTextFunction)
 
     def lineeditTextFunction(self) :
-        self.lbl_textHere.setText(self.lineEdit.text())
+        self.lineEdit_1.setText(self.lineEdit_2.text())
         #pwd_type1=(self.lineEdit.text())
         #print(pwd_type1)
         
@@ -25,12 +25,12 @@ class WindowClass(QMainWindow, form_class) :
     def printTextFunction(self) :
         #self.lineedit이름.text()
         #Lineedit에 있는 글자를 가져오는 메서드
-        print(self.lineEdit.text())
+        print(self.lineEdit_2.text())
 
     def changeTextFunction(self) :
         #self.lineedit이름.setText("String")
         #Lineedit의 글자를 바꾸는 메서드
-        pwd_type1=(self.lineEdit.text())
+        pwd_type1=(self.lineEdit_2.text())
         print(pwd_type1)
         #self.lineEdit.setText("Change Text")
         
